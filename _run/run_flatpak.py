520d982ac9a14299f1207da21f002e9d89df8f99
Amman <ammanw93@gmail.com>
2024-10-26 12:17:21 -0500

    Updated files and flatpak files

--- _run/run_flatpak.py
+++ _run/run_flatpak.py
@@ -1,5 +1,5 @@
 import os
 
-os.system('flatpak-builder build _packaging/flatpak/com.snuggerdog.pulsesmsreboot.yaml --force-clean --ccache --install --user')
+os.system('flatpak-builder build _packaging/flatpak/com.snuggerdog.pulsesmsreboot.yaml --force-clean --ccache --install --repo=repo --user')
 
 os.system('flatpak run com.snuggerdog.pulsesmsreboot')
